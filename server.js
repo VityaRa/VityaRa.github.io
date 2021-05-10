eb9640f8eb0a4a0b5ff42cff33979a2acd1b80a1
VityaRa <blackops2in@mail.ru>
2021-05-10 21:27:16 +0300

    feat: add server side

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const cheerio = require("cheerio");
+const fetch = require("node-fetch");
+let $ = require("jquery");
+
+const app = express();
+const port = 3000;
+app.use(express.static(__dirname + "/public"));
+
+app.get("/api/users", (req, res) => {
+  let info = {};
+
+  fetch("https://ru.sefon.pro/top/")
+    .then((ans) => ans.text())
+    .then((ans) => {
+      const $ = cheerio.load(ans);
+      const songs = $(".title");
+
+      let randomNumber = Math.floor(Math.random() * songs.length);
+
+      songs.each((id, value) => {
+        if (id === randomNumber) {
+          info.name = $(value).find(".artist_name a:first-child").text()
+          info.song = $(value).find(".song_name a").text()
+          res.send(JSON.stringify(info));
+
+          return
+        }
+      });
+    });
+
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
